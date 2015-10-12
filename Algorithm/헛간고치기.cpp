349505b812bc069dfd8ba3d48c1c769182fe1606
coolsik <wsw226@naver.com>
2015-10-13 06:25:54 +0900

    dp Bottom up

--- Algorithm/헛간고치기.cpp
+++ Algorithm/헛간고치기.cpp
@@ -1,41 +1,90 @@
-// Greedy로 풀기
+//// Greedy로 풀기
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//int M, S, C;
+//int barn[201], diff[201];
+//bool cmp(int a, int b) {
+//	return a > b;
+//}
+//int main() {
+//	cin >> M >> S >> C;
+//	for (int i = 0; i < C; i++) {
+//		cin >> barn[i];
+//	}
+//	sort(barn, barn + C);
+//
+//	for (int i = 1; i < C; i++) {
+//		diff[i] = barn[i] - barn[i - 1];
+//	}
+//
+//	if (M >= C) {
+//		cout << C;
+//	}
+//	else {
+//		sort(diff, diff + C, cmp);
+//
+//		int result = barn[C - 1] - barn[0] + 1;
+//		for (int i = 0; i < M - 1; i++) {
+//			result -= diff[i] - 1;
+//		}
+//		cout << result;
+//	}
+//
+//	return 0;
+//}
+
+
+
+// DP로 풀기 - bottom up
+
+//#include <iostream>
+//#include <algorithm>
+//#include <string.h>
+//using namespace std;
+//
+//int M, S, C;
+//int barn[201];
+//int d[51][201];
+//
+//int main() {
+//	cin >> M >> S >> C;
+//	for (int i = 0; i < C; i++) {
+//		cin >> barn[i];
+//	}
+//	memset(d, 0x70, sizeof(d));
+//	sort(barn, barn + C);
+//	d[0][0] = 1;
+//
+//	for (int i = 1; i < C; i++) {
+//		d[0][i] = 1 + barn[i] - barn[0];
+//		for (int j = 1; j < M; j++) {
+//			if (d[j][i]>d[j - 1][i - 1] + 1)
+//				d[j][i] = d[j - 1][i - 1] + 1;
+//			if (d[j][i]> d[j][i - 1] + barn[i] - barn[i - 1])
+//				d[j][i] = d[j][i - 1] + barn[i] - barn[i - 1];
+//		}
+//	}
+//
+//	int result = 2100000000;
+//
+//	for (int i = 0; i < M; i++) {
+//		if (result > d[i][C - 1]) {
+//			result = d[i][C - 1];
+//		}
+//	}
+//
+//	cout << result;
+//
+//
+//
+//	return 0;
+//}
+
+// DP로 풀기 - top down
+
 #include <iostream>
 #include <algorithm>
-using namespace std;
-int M, S, C;
-int barn[201], diff[201];
-bool cmp(int a, int b) {
-	return a > b;
-}
-int main() {
-	cin >> M >> S >> C;
-	for (int i = 0; i < C; i++) {
-		cin >> barn[i];
-	}
-	sort(barn, barn + C);
-
-	for (int i = 1; i < C; i++) {
-		diff[i] = barn[i] - barn[i - 1];
-	}
-
-	if (M >= C) {
-		cout << C;
-	}
-	else {
-		sort(diff, diff + C, cmp);
-
-		int result = barn[C - 1] - barn[0] + 1;
-		for (int i = 0; i < M - 1; i++) {
-			result -= diff[i] - 1;
-		}
-		cout << result;
-	}
-	return 0;
-}
-
-
-/*
-#include <iostream>
 using namespace std;
 #define FAIL 999999999
 
@@ -62,15 +111,14 @@ int func(int i, int j) {
 
 
 int main() {
-	//f(i, j) = (새로 덮는경우, 연장하는경우) 판자의 갯수 i, j번째까지
-	//memset(d, -1, sizeof(d));
 	cin >> M >> S >> C;
 	for (int i = 0; i < C; i++) {
 		cin >> barn[i];
 	}
-	cout << func(0, 0);
+	sort(barn, barn + C);
+	d[0][0] = 1;
+
+	
 
 	return 0;
 }
-
-*/
